2aceff7c8b53d01bc6ac33eb11e411ce49242b7e
jahrulezfrancis <longsaarmuknaan@gmail.com>
2025-10-12 23:45:56 +0100

    adding collection fetch helpers

---
+++ lib/fetch-collections.ts
@@ -0,0 +1,33 @@
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "./firebase"
+import type { BlogPost, EventType, GalleryImage, programType, ProjectType, TeamMember } from "@/lib/types"
+
+export async function fetchBlogs(): Promise<BlogPost[]> {
+  const snapshot = await getDocs(collection(db, "blogPosts"))
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as BlogPost[]
+}
+
+export async function fetchEvents(): Promise<EventType[]> {
+  const snapshot = await getDocs(collection(db, "events"))
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as EventType[]
+}
+
+export async function fetchProjects(): Promise<ProjectType[]> {
+  const snapshot = await getDocs(collection(db, "projects"))
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as ProjectType[]
+}
+
+export async function fetchTeam(): Promise<TeamMember[]> {
+  const snapshot = await getDocs(collection(db, "team"))
+  return snapshot.docs.map((doc) => ({ ...doc.data() })) as TeamMember[]
+}
+
+export async function fetchGallery(): Promise<GalleryImage[]> {
+  const snapshot = await getDocs(collection(db, "gallery"))
+  return snapshot.docs.map((doc) => ({ ...doc.data() })) as GalleryImage[]
+}
+
+export async function fetchPrograms(): Promise<programType[]> {
+  const snapshot = await getDocs(collection(db, "programs"))
+  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as programType[]
+}
